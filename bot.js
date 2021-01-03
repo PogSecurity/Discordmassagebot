efb87bb1548a2cbb00ca46bee9fd958a9a83a894
PogSecurity <fener_samet_2001@hotmail.com>
2021-01-03 08:40:40 +0300

    project pertama

---
+++ bot.js
@@ -0,0 +1,17 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+client.on('ready', () => {
+  console.log(`Botunuz ${client.user.tag} artık aktif halde!`);
+});
+
+client.on('message', msg => {
+  if (msg.content.toLowerCase() === 'sa') {
+    msg.reply('Hoş geldin dostum seni görmek ne güzel!');
+  }
+  if (msg.content.toLowerCase() === 'instagram') {
+    msg.reply('@samet2gether');
+  }
+});
+
+client.login('Nzk1MTUxOTQ4ODk4NTAwNjA5.X_FM9Q.zP9GhwdM_F2yec7x7hj0azc4dz8');
